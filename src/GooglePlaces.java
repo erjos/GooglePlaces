a3741ee85d92ebed6ac172791a99cd9ff8f0de98
Ethan Joseph <ethanrj91@gmail.com>
2016-07-21 12:41:41 -0400

    making changes to display the hours on google places that correspond the the current day that the user wants to access the data.

--- src/GooglePlaces.java
+++ src/GooglePlaces.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -111,10 +113,19 @@ public class GooglePlaces {
 			day = jarray.get(i);
 			
 			System.out.println(day);
+			
+			Calendar date = Calendar.getInstance();
+			int dayOfweek = date.get(Calendar.DAY_OF_WEEK);
+			
+			System.out.println(dayOfweek);
+			
+			//could use a switch statement or something else to check the day of the week and just display that day to the user on the page
+			
 			}
 			// JSONObject obj = new JSONObject(line);
 
 			// JsonParser parser = new JsonParser();
+			
 			// JsonElement element = parser.parse(line);
 		}
 
